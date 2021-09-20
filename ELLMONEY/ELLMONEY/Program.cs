cd7fd949319564ddd5d35b6eb0499adfde3402d3
Travis Marcotte <travis.marcotte173@gmail.com>
2021-09-20 14:51:46 -0600

    l

--- ELLMONEY/ELLMONEY/Program.cs
+++ ELLMONEY/ELLMONEY/Program.cs
@@ -31,26 +31,57 @@ namespace ELLMONEY
         private static void Begin()
         {
             Console.Clear();
-            Write.Line("[1] Add Money to everyone");
-            Write.Line("[2] Take Money from everyone");
+            Write.Line("[1] Add Money");
+            Write.Line("[2] Take Money");
             Write.Line("[D] See everyone's money");
 
-            string x = Return.Option();
+            string x = Return.Option();            
             if (x == "1")
             {
-                Write.Line("How much would you like to give each student?");
+                bool multi = Choose(true);
+                Write.Line("How much would you like to give?");
                 int y = Return.Integer();
-                foreach (Student s in students) Add(s, y);
+                if (multi)
+                {
+                    foreach (Student s in students) Add(s, y);
+                    Write.Line($"{y} dollars has been given to each student");
+                }
+                else 
+                {
+                    Student s = GetStudent();
+                }
             }
             else if (x == "2")
             {
-                Write.Line("How much would you like to take from each student?");
+                bool multi = Choose(false);
+                Write.Line("How much would you like to take");
                 int y = Return.Integer();
-                foreach (Student s in students) Subtract(s, y);
+                if (multi)
+                {
+                    foreach (Student s in students) Subtract(s, y);
+                    Write.Line($"{y} dollars has been taken from each student");
+                }
             }
             else if (x == "d") Display();
             Write.KeyPress();
             Begin();
+        }
+
+        private static Student GetStudent()
+        {
+            
+        }
+
+        private static bool Choose(bool give)
+        {
+            Console.Clear();
+            string x = (give) ? "give to" : "take from";
+            Write.Line($"Would you like to {x} [1] a student or [2] the class?");
+            string y = Return.Option();
+            if (y == "1") return false;
+            else if (y == "2") return true;
+            else Choose(give);
+            return true;
         }
 
         private static void Add(Student s, int y) => s.money += y;
@@ -59,8 +90,6 @@ namespace ELLMONEY
         public static void Display()
         {
             foreach (Student s in students) Console.WriteLine($"{s.name}: {s.money}$");
-        }
-
-        
+        }        
     }
 }
